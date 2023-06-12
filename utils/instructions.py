61170b7a7dd2b5a8ee22646865f4fd76699f139e
RamosQuim <qlramos@up.edu.ph>
2023-06-12 12:04:00 +0800

    feat: added app instructions

--- utils/instructions.py
+++ utils/instructions.py
@@ -1,2 +1,48 @@
 def instructions():
-        print("This is for instructions section")
+        print(
+    '''
+,---------------------------------------,
+| █░█ █▀█ █░█░█  ▀█▀ █▀█  █░█ █▀ █▀▀ ▀█ |
+| █▀█ █▄█ ▀▄▀▄▀  ░█░ █▄█  █▄█ ▄█ ██▄ ░▄ |
+'---------------------------------------' '''
+        )
+        print(
+'''
+------------------🅶 🆄 🅸 🅳 🅴-----------------
+Splitwise is an app for dividing expenses 
+with your friends. It allows you and your 
+friends to input multiple bills and keep 
+track of who owes who, and then it assists 
+you in settling up with each other. Here's 
+a basic rundown of how it works.
+
+➢ Firstly, add friends into your list by
+  going to the FRIENDS SECTION.
+
+➢ To add an expense with your friends, go
+  to EXPENSES SECTION. You may add an
+  expense with your friend only or with
+  your group.
+
+➢ Should you want to manage expenses by
+  more than 1 friend, proceed to FROUPS
+  SECTION. Add a group and assign your
+  friends to that group.
+
+➢ Navigate to PAYMENT SECTION to settle
+  balances with your friend or your group.
+
+➢ Want to check the progress of every
+  balance? Go to REPORTS SECTIONS to see
+  the updated version of your transaction
+  within your friend or your group.
+
+And you're done! You are now ready to use
+this application. Have fun splitting!~
+
+Feeling lost?
+If you are not ready yet to explore, do
+not hesitate to re-read this guide.
+--------------------------------------------
+'''
+        )
